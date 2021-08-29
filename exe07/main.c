e62d30246a8c2913a6e76c875939a04b40d59d0d
Rafael Henrique da Rocha Silva <48327439+rafaelhrocha@users.noreply.github.com>
2021-08-29 23:10:20 +0000

    att

--- exe07/main.c
+++ exe07/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
-}
+	int n, max, min;
+	scanf("%d", &n);
+
+	int v[n];
+
+	for( int i = 0; i<n; i++){
+		scanf("%d",&v[i]);
+		if( i == 0){
+		    min = v[i];
+		    max = v[i];
+		}
+		
+		if( v[i] > max){
+			max = v[i];
+		}
+		if( v[i] < min){
+			min = v[i];
+		}
+	}
+	printf("MENOR = %d\nMAIOR = %d", min, max);
+	return 0;
+	}
--- exe09/main.c
+++ exe09/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
+   int n, max1, max2, max3;
    scanf("%i %i", &n1, &n2);
    printf("SOMA = %i\n", n1+n2);
    return 0;
